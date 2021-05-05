831a286baa0c96456bfb07443833dd6a7cd61548
Laavart <66962533+laavart@users.noreply.github.com>
2021-05-05 11:40:45 +0530

    Add files via upload

---
+++ Scripts/indexscript.js
@@ -0,0 +1,32 @@
+const speechRecognition = window.webkitSpeechRecognition;
+const recog = new speechRecognition();
+
+function Speak(TEXT) {
+    const utter = new SpeechSynthesisUtterance();
+    utter.text = TEXT;
+    utter.rate=0.85;
+    utter.lang='en-US';
+    window.speechSynthesis.speak(utter);
+}
+
+Speak("Hello! Gamers,");
+Speak("Welcome to Colosseum ");
+Speak("say Start to Begin");
+
+function startListening() {
+    recog.continuous = true;//
+    recog.start();
+    recog.onstart = function (){
+        console.log("Started Listenig..")
+    };
+    recog.onresult = function (data){
+        let text = data.results[0][0].transcript;
+        text = text.toLowerCase();
+        if (text.includes("start")) {
+            window.location.replace("Pages/home.html");
+        }
+        else Speak("Sorry I can't get you!");
+    };
+}
+
+startListening();
